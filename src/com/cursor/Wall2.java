96a22950037722a07c83b6e9544d09e96575ff7a
Stepan Dulka <fotosteuge@gmail.com>
2021-12-10 00:56:30 +0200

    Add Walls and refactored GameController

---
+++ src/com/cursor/Wall2.java
@@ -0,0 +1,9 @@
+package com.cursor;
+
+import com.cursor.gameobjects.ObjectOnScreen;
+
+public class Wall2 extends ObjectOnScreen {
+    public Wall2(int x, int y) {
+        super(x,y,'_');
+    }
+}
